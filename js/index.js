9e8c1e2ff3bcc3bee0648ee292add866f02cde95
Mikhailo.Yu <miha.jewish@gmail.com>
2023-01-21 16:33:29 +0200

    init

--- js/index.js
+++ js/index.js
@@ -3,11 +3,10 @@ const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 function generateKey (length, characters) {
     let result = '';
     for (i = 0; i <= length; i++) {
-       characters.charAt(Math.floor(Math.random() * 37));
        result = result + characters.charAt(Math.floor(Math.random() * 37));       
     }
     return result;
 }
 
-const key = generateKey(16, characters);
+const key = generateKey(10, characters);
 console.log(key);
